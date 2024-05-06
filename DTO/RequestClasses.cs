6197ade50bbe2fdaa9e9a7a2cf29be0068c2b0ab
kokeiro001 <kokeiro.ssbm.001@gmail.com>
2024-05-06 12:40:07 +0900

    リクエスト系の検証用定義のファイルを分けた

---
+++ DTO/RequestClasses.cs
@@ -0,0 +1,40 @@
+﻿namespace DTO
+{
+
+    public abstract class RequestBase : DTOBase
+    {
+    }
+
+    public enum RequestResult
+    {
+        /// <summary>成功</summary>
+        Success = 0,
+
+        /// <summary>失敗</summary>
+        Failure = 10,
+
+        /// <summary>金欠</summary>
+        NotEnoughMoney = 20,
+
+        /// <summary>食べ物が見つからない</summary>
+        NotFoundFood = 30,
+    }
+
+    /// <summary>
+    /// 食べ物リクエスト。
+    /// あれも食べたい。
+    /// これも食べたい。
+    /// <results>
+    ///   <result><see cref="RequestResult.NotEnoughMoney"/></result>
+    ///   <result><see cref="RequestResult.NotFoundFood"/></result>
+    /// </results>
+    /// </summary>
+    public class RequestFood : RequestBase
+    {
+        /// <summary>
+        /// 食べ物名。
+        /// 一品までだよ。
+        /// </summary>
+        public string FoodName { get; set; } = "";
+    }
+}
