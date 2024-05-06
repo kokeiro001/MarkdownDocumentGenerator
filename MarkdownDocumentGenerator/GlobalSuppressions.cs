b9a827a1d2a6385cb277037be47de9549bd8f517
kokeiro001 <kokeiro.ssbm.001@gmail.com>
2024-05-06 12:31:51 +0900

    抑制追加した

--- MarkdownDocumentGenerator/GlobalSuppressions.cs
+++ MarkdownDocumentGenerator/GlobalSuppressions.cs
@@ -1,4 +1,9 @@
-﻿// This file is used by Code Analysis to maintain SuppressMessage
+﻿using System.Diagnostics.CodeAnalysis;
+
+[assembly: SuppressMessage("Style", "IDE0042:変数の宣言を分解", Justification = "<保留中>", Scope = "member", Target = "~M:MarkdownDocumentGenerator.Renderer.DefaultMarkdownRenderer.RenderMarkdown(MarkdownDocumentGenerator.TypeInfo,System.String,System.String)~System.Threading.Tasks.Task")]
+[assembly: SuppressMessage("Style", "IDE0057:範囲演算子を使用", Justification = "<保留中>", Scope = "member", Target = "~M:MarkdownDocumentGenerator.StringExtension.TrimStart(System.String,System.String)~System.String")]
+[assembly: SuppressMessage("Performance", "CA1822:メンバーを static に設定します", Justification = "<保留中>", Scope = "member", Target = "~M:MarkdownDocumentGenerator.Renderer.DefaultMarkdownRenderer.RenderMarkdown(MarkdownDocumentGenerator.TypeInfo,System.String,System.String)~System.Threading.Tasks.Task")]
+// This file is used by Code Analysis to maintain SuppressMessage
 // attributes that are applied to this project.
 // Project-level suppressions either have no target or are given
 // a specific target and scoped to a namespace, type, member, etc.
